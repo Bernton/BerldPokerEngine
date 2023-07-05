f36a81968f19e58167b86c79e4e8d6603da0e043
Bernton <nielsen.bernt@hotmail.com>
2023-07-05 17:41:29 +0200

    add distinct holding to ouput project and rework

---
+++ ConsoleAppOutput/DistinctHolding.cs
@@ -0,0 +1,63 @@
+﻿using BerldPokerEngine.Poker;
+using System.Text;
+
+namespace ConsoleAppOutput
+{
+    internal class DistinctHolding
+    {
+        internal Card[] Cards { get; }
+        internal string Key { get; }
+        internal int Frequency { get; set; }
+
+        internal DistinctHolding(Card[] cards)
+        {
+            Span<bool> wasAssigned = stackalloc bool[cards.Length];
+            Card[] normalCards = new Card[cards.Length];
+
+            int currentSuit = Suit.Clubs;
+
+            for (int i = 0; i < cards.Length; i++)
+            {
+                if (wasAssigned[i])
+                {
+                    continue;
+                }
+
+                int suitToMap = cards[i].Suit;
+
+                for (int j = i; j < cards.Length; j++)
+                {
+                    if (cards[j].Suit == suitToMap)
+                    {
+                        wasAssigned[j] = true;
+                        normalCards[j] = Card.Create(cards[j].Rank, currentSuit);
+                    }
+                }
+
+                currentSuit++;
+            }
+
+            Array.Sort(normalCards);
+
+            Cards = normalCards;
+            Key = GetKey(normalCards);
+        }
+
+        private static string GetKey(Card[] cards)
+        {
+            StringBuilder builder = new();
+
+            foreach (Card card in cards)
+            {
+                builder.Append(card.ToString());
+            }
+
+            return builder.ToString();
+        }
+
+        public override string ToString()
+        {
+            return $"{Key} {Frequency}";
+        }
+    }
+}
---
+++ ConsoleAppOutput/DistinctHoldingCalculator.cs
@@ -0,0 +1,64 @@
+﻿using BerldPokerEngine.Poker;
+using BerldPokerEngine;
+
+namespace ConsoleAppOutput
+{
+    internal static class DistinctHoldingCalculator
+    {
+        internal static void Start()
+        {
+            List<Card> cards = EngineData.GetAllCards();
+
+            Dictionary<string, DistinctHolding> holdingMap = new();
+
+            Card[] holdingCards = new Card[5];
+
+            for (int c0 = 0; c0 < cards.Count; c0++)
+            {
+                holdingCards[0] = cards[c0];
+
+                for (int c1 = c0 + 1; c1 < cards.Count; c1++)
+                {
+                    holdingCards[1] = cards[c1];
+
+                    for (int c2 = c1 + 1; c2 < cards.Count; c2++)
+                    {
+                        holdingCards[2] = cards[c2];
+
+                        for (int c3 = c2 + 1; c3 < cards.Count; c3++)
+                        {
+                            holdingCards[3] = cards[c3];
+
+                            for (int c4 = c3 + 1; c4 < cards.Count; c4++)
+                            {
+                                holdingCards[4] = cards[c4];
+
+
+                                DistinctHolding holding = new(holdingCards);
+
+                                if (holdingMap.ContainsKey(holding.Key))
+                                {
+                                    holdingMap[holding.Key].Frequency++;
+                                }
+                                else
+                                {
+                                    holding.Frequency = 1;
+                                    holdingMap.Add(holding.Key, holding);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            DistinctHolding[] holdings = holdingMap.Values.ToArray();
+
+
+
+            int totalFrequency = holdings.Sum(c => c.Frequency);
+
+            Console.WriteLine($"Amount of distinct holdings: {holdings.Length} (should be 134459)");
+            Console.WriteLine($"Total frequency: {totalFrequency}");
+        }
+    }
+}
