852abc90127ad9c7a35d93fea114ef5fd881d883
Bernton <nielsen.bernt@hotmail.com>
2023-07-05 14:13:42 +0200

    add skip further progress

--- TexasHoldemBonusSimulator/Engines/TreeEngine.cs
+++ TexasHoldemBonusSimulator/Engines/TreeEngine.cs
@@ -49,7 +49,7 @@ namespace TexasHoldemBonusSimulator.Engines
 
             long winnings = 0;
 
-            foreach (DistinctHolding holding in holdingMap.Values.Skip(88))
+            foreach (DistinctHolding holding in holdingMap.Values.Skip(98))
             {
                 List<Card> playerCards = new()
                 {
